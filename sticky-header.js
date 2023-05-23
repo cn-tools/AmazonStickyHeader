355e21f733e443add01e8ebdbb76fd3f6871fddd
Clemens Neubauer <cn-tools@gmx.at>
2023-05-23 14:57:13 +0200

    flyout cart

--- sticky-header.js
+++ sticky-header.js
@@ -7,21 +7,34 @@ function getState(callback){
 function toggleStickyHeader(state){
     // add/remove css class to header
     var elem = document.querySelector('header');
-    var toggle = state ? elem.classList.add : elem.classList.remove;
-    toggle.call(elem.classList, 'sticky_header');
+    if (elem != null) {
+        var toggle = state ? elem.classList.add : elem.classList.remove;
+        toggle.call(elem.classList, 'sticky_header');
 
-    // insert div element for helping to sticky header
-    lDummyId = 'AmazonStickyHeaderHelper';
-    var dummy_div = document.getElementById(lDummyId);
-    if (dummy_div == null) {
-        var dummy_div = document.createElement('div');
-        dummy_div.id = lDummyId;
-        elem.parentNode.insertBefore(dummy_div, elem.nextSibling);
+        // insert div element for helping to sticky header
+        lDummyId = 'AmazonStickyHeaderHelper';
+        var dummy_div = document.getElementById(lDummyId);
+        if (dummy_div == null) {
+            var dummy_div = document.createElement('div');
+            dummy_div.id = lDummyId;
+            elem.parentNode.insertBefore(dummy_div, elem.nextSibling);
+        }
+
+        // set height for helping div elem if needed
+        var lHeight = state ? '' + elem.offsetHeight + 'px' : '';
+        dummy_div.style.marginTop = lHeight;
+
+        // adapt header width around vertical cart bar
+        var lFlyOutEwc = document.getElementById('nav-flyout-ewc');
+        if (lFlyOutEwc != null) {
+            if (state === true) {
+                var lHeaderWidth = lHeaderWidth - lFlyOutEwc.offsetWidth;
+                elem.style.width = '' + lHeaderWidth + 'px';
+            } else {
+                elem.style.width = '' + dummy_div.offsetWidth + 'px';
+            }
+        }
     }
-
-    // set height for helping div elem if needed
-    var lHeight = state ? '' + elem.offsetHeight + 'px' : '';
-    dummy_div.style.marginTop = lHeight;
 }
 
 chrome.storage.onChanged.addListener(function (data) {
